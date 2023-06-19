2d440761bba8ab781f8a79d3ddea0ffc720c744d
Tim Hacker <tim.hacker@madetech.com>
2023-06-19 17:23:13 +0100

    Initial C# commit with requirements

---
+++ GildedRose/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+namespace GildedRoseKata
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+
+        // public Item(string name, int sellIn, int quality)
+        // {
+        //     this.Name = name;
+        //     this.SellIn = sellIn;
+        //     this.Quality = quality;
+        // }
+
+        //private Item blah = new Item("blah", 10, 23);
+    }
+}
